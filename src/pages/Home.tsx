6628821b4f1c16ea70cdf80f0ac55166393c0dfb
Bruncky <brunobrunckhorst@gmail.com>
2020-04-06 03:33:56 +0200

    Possible IAB fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import { Plugins } from '@capacitor/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 const { Browser } = Plugins;
 
 const Home: React.FC = () => {
-    Browser.open({ url: 'https://app.queroajudar.org/', windowName: "_self" });
 
     return (
         <IonPage>
+            <IonContent>
+                { Browser.open({ url: 'https://app.queroajudar.org/', windowName: "_self" }) }
+            </IonContent>
         </IonPage>
     );
 };
