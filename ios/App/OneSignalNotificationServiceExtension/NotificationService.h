5583d07a565cf7c405ce64224b89dec0699ae9b0
Bruncky <brunobrunckhorst@gmail.com>
2020-04-13 19:46:56 +0200

    Added corrected setup files

---
+++ ios/App/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Bruno Andrade Brunckhorst on 13.04.20.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
