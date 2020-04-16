101a6251437f933d425f2afba202fecad526819a
Bruncky <brunobrunckhorst@gmail.com>
2020-04-16 19:50:36 +0200

    Added call to statusbar component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import { IonContent, IonPage } from '@ionic/react';
 import React from 'react';
-import { loadIAB } from '../components/InAppBrowser'
+import { loadIAB } from '../components/InAppBrowser';
+import { changeStatusbar } from '../components/StatusBar';
 import './Home.css';
 
 const Home: React.FC = () => {
     return (
         <IonPage>
             <IonContent>
+                { [changeStatusbar()] }
                 { [loadIAB()] }
             </IonContent>
         </IonPage>
